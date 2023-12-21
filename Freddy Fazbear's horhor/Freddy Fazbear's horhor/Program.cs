3d2422d00fe2d9d1b9e602f1eb8c192b6e36640c
mr.fouse <pavelknyazevvv@gmail.com>
2023-12-21 15:04:48 +0300

    ерок

---
+++ Freddy Fazbear's horhor/Freddy Fazbear's horhor/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+
+class food
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+
+    public food(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"Блюдо: {Name}");
+        Console.WriteLine($"Описание: {Description}");
+    }
+}
+
+
+
+class pizza : food
+{
+    public pizza(string name, string description) : base(name, description)
+    {
+    }
+
+    public void Nyam()
+    {
+        Console.WriteLine($"С эксклюзивной арбузной курочкой, ваша {Name}!");
+    }
+}
+
+class borga : food
+{
+    public borga(string name, string description) : base(name, description)
+    {
+    }
+
+    public void Nom()
+    {
+        Console.WriteLine($"Когда поедаете {Name} следите за своими лобными долями.");
+    }
+}
+
+class dessert : food
+{
+    public dessert(string name, string description) : base(name, description)
+    {
+    }
+
+    public void Chomp()
+    {
+        Console.WriteLine($"Без {Name} уже не так весело!");
+    }
+}
+
+class drink : food
+{
+    public drink(string name, string description) : base(name, description)
+    {
+    }
+
+    public void Slurp()
+    {
+        Console.WriteLine($"Пей {Name} до дна! (будь мужиком)");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<food> menu = new List<food>
+        {
+            new pizza("Супер пицца 'У Чики'", "За качество не ручаемся."),
+            new borga("Бургер 'Фредбер'", "Невероятный вкус в каждом УКУСЕ :DDDDDD."),
+            new dessert("Десерт 'Лицо Бонни'", "МММ МЕТАЛЛ."),
+            new drink("Напиток 'Фохе'", "Освежающая гармония пиратского пота.")
+        };
+
+        foreach (food item in menu)
+        {
+            item.Display();
+
+            if (item is pizza pizza)
+            {
+                pizza.Nyam();
+            }
+            else if (item is borga burger)
+            {
+                burger.Nom();
+            }
+            else if (item is dessert dessert)
+            {
+                dessert.Chomp();
+            }
+            else if (item is drink drink)
+            {
+                drink.Slurp();
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ garazh/garazh/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+class hehicle
+{
+    public string Name { get; set; }
+
+    public hehicle(string name)
+    {
+        Name = name;
+    }
+
+    public virtual void StartEngine()
+    {
+        Console.WriteLine($"Включаем транспортное средство: {Name}");
+    }
+}
+
+class land : hehicle
+{
+    public land(string name) : base(name)
+    {
+    }
+
+    public override void StartEngine()
+    {
+        base.StartEngine();
+        Console.WriteLine("Бипбип поехали.");
+    }
+}
+
+class water : hehicle
+{
+    public water(string name) : base(name)
+    {
+    }
+
+    public override void StartEngine()
+    {
+        base.StartEngine();
+        Console.WriteLine("Бульбуль поплыли.");
+    }
+}
+
+class submarine : water
+{
+    public submarine(string name) : base(name)
+    {
+    }
+
+    public override void StartEngine()
+    {
+        base.StartEngine();
+        Console.WriteLine("Погрузились под воду и готовы делать подбульподбуль.");
+    }
+}
+
+class air : hehicle
+{
+    public air(string name) : base(name)
+    {
+    }
+
+    public override void StartEngine()
+    {
+        base.StartEngine();
+        Console.WriteLine("ВЖЖЖЖЖЖЖЖЖЖЖЖЖЖЖЖЖ полетели.");
+    }
+}
+
+class underground : hehicle
+{
+    public underground(string name) : base(name)
+    {
+    }
+
+    public override void StartEngine()
+    {
+        base.StartEngine();
+        Console.WriteLine("Какие звуки издаются под землей?");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        hehicle[] vehicles = new hehicle[]
+        {
+            new land("СуперМашина2281488"),
+            new water("Шлюпка"),
+            new submarine("СУперсегадуперсубмарина для потопления врагов"),
+            new air("Истребитель"),
+            new underground("Метро")
+        };
+
+        foreach (var vehicle in vehicles)
+        {
+            vehicle.StartEngine();
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
+    }
+}
